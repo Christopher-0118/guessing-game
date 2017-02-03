75001bef53df87d4092b38dd6066080e20a5af82
Christopher-0118 <k.karantsevich@gmail.com>
2017-02-03 18:22:58 +0300

    guessing-game: implement guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,26 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.arr = [];
+        this.length = max;
+        this.min = min;
+        
+        for (min; min <= max; min++) {
+           this.arr.push(min);
+        }
     }
 
     guess() {
-
+        this.mid = Math.round((this.length + this.min) / 2);
+        return this.arr[this.mid];
     }
 
     lower() {
-
+        this.length = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
